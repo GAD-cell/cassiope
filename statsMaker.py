f2f2884d59365f0fbbdfda2c80f2e93ddc967087
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-03-25 00:25:15 +0100

    Update file paths and add filter for database entry keys

--- statsMaker.py
+++ statsMaker.py
@@ -1,9 +1,9 @@
 """
 Takes a database of papers in json format, processes them with util/paperStats/paperStats.py and outputs a CSV with the statistics of each paper.
 
-It is expected that the paper files have been downloaded with util/Download Papers/downloadPapers.py.
-PDFs should be in util/Download Papers/PDF and zipped LaTeX projets in util/Download Papers/LaTeX.
-Their filesnames should be {title}.pdf and {title}.zip respectively.
+It is expected that the paper files have been downloaded with util/downloadPapers/downloadPapers.py.
+PDFs should be in util/downloadPapers/PDF and zipped LaTeX projets in util/downloadPapers/LaTeX.
+Their filesnames should be {title}.pdf and {title}.tar.gz respectively.
 
 Sample database entry :
 
@@ -30,37 +30,60 @@ Sample database entry :
 
 import json
 import tempfile
-import zipfile
+import tarfile
 from util.paperStats import paperStats
 
-database = "util/Download Papers/database_filtered.json"
+database = "util/downloadPapers/database_filtered.json"
 output = "STATS.csv"
+
+
+def filterDatabaseEntryKeys(entry):
+    return {
+        "corpusid": entry["corpusid"],
+        "arxiv_id": entry["externalids"]["ArXiv"],
+        "title": entry["title"],
+        "year": entry["year"],
+        "venue": entry["venue"],
+        "citationcount": entry["citationcount"],
+        "referencecount": entry["referencecount"],
+        "influentialcitationcount": entry["influentialcitationcount"],
+    }
+
 
 with open(database, "r") as file:
     lines = file.readlines()
 
     # Itération sur chaque ligne du fichier JSON
     for index, line in enumerate(lines, start=0):
-
         try:
-            paper_data = json.loads(line)
+            semantic_scholar_data = json.loads(line)
 
             print(
-                f"Processing file util/Download Papers/LaTeX/{paper_data['title']}.zip"
+                f"Processing file util/downloadPapers/LaTeX/{semantic_scholar_data['title']}.zip"
             )
 
-            pdf_path = f"util/Download Papers/PDF/{paper_data['title']}.pdf"
-            latex_path = f"util/Download Papers/LaTeX/{paper_data['title']}.zip"
+            pdf_path = f"util/downloadPapers/PDF/{semantic_scholar_data['title']}.pdf"
+            latex_path = (
+                f"util/downloadPapers/LaTeX/{semantic_scholar_data['title']}.tar.gz"
+            )
 
             # Unzip the LaTeX project to a temporary directory
             with tempfile.TemporaryDirectory() as tempdir:
-                with zipfile.ZipFile(latex_path, "r") as zip_ref:
-                    zip_ref.extractall(tempdir)
+                try:
+                    with tarfile.open(latex_path, "r:gz") as tar:
+                        tar.extractall(tempdir)
 
-                # Run paperStats.py
-                stats = paperStats.paperStats(pdf_path, tempdir)
+                except FileNotFoundError:
+                    continue
 
-                print(stats)
+                paper_stats = paperStats.paperStats(pdf_path, tempdir)
+
+            filtered_semantic_scholar_data = filterDatabaseEntryKeys(
+                semantic_scholar_data
+            )
+            all_stats = filtered_semantic_scholar_data | paper_stats
+
+            print(json.dumps(all_stats, indent=4))
 
         except json.JSONDecodeError:
             print(f"Erreur de décodage JSON sur la ligne {index}: {line}")
