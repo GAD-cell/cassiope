792eadb60631552b665a8ec2114dd06db69da787
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-05-12 19:13:45 +0200

    Take care of dependencies + extra formatting

--- util/topicModeling/topicModeling.py
+++ util/topicModeling/topicModeling.py
@@ -5,14 +5,16 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from collections import defaultdict
 
+
 # Fonction pour extraire le texte à partir d'un fichier PDF
 def extract_text_from_pdf(pdf_file):
     text = ""
-    with open(pdf_file, 'rb') as file:
+    with open(pdf_file, "rb") as file:
         pdf_reader = PyPDF2.PdfReader(file)
         for page_num in range(len(pdf_reader.pages)):
             text += pdf_reader.pages[page_num].extract_text()
     return text
+
 
 # Fonction pour nettoyer et prétraiter le texte
 def preprocess_text(text):
@@ -20,7 +22,8 @@ def preprocess_text(text):
     tokens = word_tokenize(text)
     # Suppression de la ponctuation
     tokens = [word.lower() for word in tokens if word.isalpha()]
-    return ' '.join(tokens)
+    return " ".join(tokens)
+
 
 # Fonction pour appliquer le modèle de topic modeling (NMF)
 def apply_nmf_model(text, num_topics=5):
@@ -30,15 +33,19 @@ def apply_nmf_model(text, num_topics=5):
     nmf.fit(X)
     return nmf, vectorizer
 
+
 # Fonction pour obtenir les sujets les plus traités
 def get_top_topics(nmf_model, vectorizer, top_n=3):
     feature_names = vectorizer.get_feature_names_out()
     topics = nmf_model.components_
     top_topics = []
     for topic_idx in range(len(topics)):
-        topic_words = [feature_names[i] for i in topics[topic_idx].argsort()[:-top_n - 1:-1]]
+        topic_words = [
+            feature_names[i] for i in topics[topic_idx].argsort()[: -top_n - 1 : -1]
+        ]
         top_topics.append(topic_words)
     return top_topics
+
 
 # Fonction principale
 def main(pdf_file):
@@ -52,8 +59,23 @@ def main(pdf_file):
     top_topics = get_top_topics(nmf_model, vectorizer)
     return top_topics
 
+
 # Exemple d'utilisation
 if __name__ == "__main__":
+
+    # Check if punkt and stopwords are downloaded
+    import nltk
+
+    try:
+        nltk.data.find("tokenizers/punkt")
+    except LookupError:
+        nltk.download("punkt")
+
+    try:
+        nltk.data.find("corpora/stopwords")
+    except LookupError:
+        nltk.download("stopwords")
+
     pdf_file = "2403.06080v1.pdf"  # Spécifiez le chemin vers votre fichier PDF
     top_topics = main(pdf_file)
     print("Top 3 sujets les plus traités :")
