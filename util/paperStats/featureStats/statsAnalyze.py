9d779e486b256df8bcb4cd2ca6f463a3cefd1009
GAD <GAD-cell>
2024-06-02 23:54:17 +0200

    prediction par fôret aléatoire

--- util/paperStats/featureStats/statsAnalyze.py
+++ util/paperStats/featureStats/statsAnalyze.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn import tree
-from sklearn.ensemble import RandomForestRegressor
 from sklearn import linear_model
 import statsmodels.api as sm
 from math import*
@@ -10,7 +9,8 @@ import seaborn as sns
 import csv
 import ast
 
-
+from sklearn.metrics import mean_absolute_error
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.neighbors import KNeighborsRegressor
@@ -60,6 +60,7 @@ def visualize_features(df):
             plt.hist(df[col],alpha=0.6,facecolor='g')
             plt.savefig("./figures/"+col)
 
+#estimation de l'importance relative de chacunes des features
 def randomForest(X,y,df):
     #tree_model = tree.DecisionTreeRegressor(max_depth=10,random_state=None)
     #tree_model.fit(X,y)
@@ -90,6 +91,7 @@ def correlation_matrix(df):
     sns.heatmap(df.corr(method="pearson"), cmap = 'coolwarm',vmin=-1,vmax=1,center=0)
     plt.show()
 
+#calcul du nomnbre de citations par topic pour évaluer leurs popularités
 def gen_citationcount_per_topic():
     bertopic_df=pd.read_csv('/home/gad/Documents/cassiope/cassiope/util/topicModeling/bertopic/BERTopic_modified.csv')
     stat_df = pd.read_csv('/home/gad/Documents/cassiope/cassiope/util/paperStats/STATS.csv')
@@ -107,7 +109,7 @@ def gen_citationcount_per_topic():
     bertopic_df.to_csv('/home/gad/Documents/cassiope/cassiope/util/paperStats/STATS_topic.csv', index=False)
     return citationcount_per_topic
 
-
+#calcul des modèles régressifs
 def gen_regressor_model():
 
 
@@ -185,6 +187,7 @@ def gen_regressor_model():
 
     return regression_results
 
+#prediction par régression
 def citationcount_predict():
     regression_results = gen_regressor_model()
 
@@ -208,6 +211,29 @@ def citationcount_predict():
     print('prediction :' +str(final_predictions))
     return final_predictions
 
+#prédiction par foret aléatoire
+def random_forest():
+    data,citationcount = read_csv('/home/gad/Documents/cassiope/cassiope/util/paperStats/STATS.csv')
+    data['citationcount']=citationcount
+
+    X = data.drop(columns=['citationcount']) 
+    y = data['citationcount']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Initialiser le modèle de forêt aléatoire
+    random_forest = RandomForestRegressor(n_estimators=1000, random_state=42)  # Nombre d'arbres = 100
+    random_forest.fit(X_train, y_train)
+    y_pred = random_forest.predict(X_test)
+
+    # Calculer l'erreur quadratique moyenne (RMSE)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    mae = mean_absolute_error(y_test, y_pred)
+
+    erreur_percent = (mae / y_test.mean()) * 100
+    print("erreur",erreur_percent )
+    print("RMSE:", rmse)
+
 if __name__=="__main__":
     #data,citationcount = read_csv("../STATS.csv")
     #plot('content_references','citationcount',data)
@@ -221,4 +247,5 @@ if __name__=="__main__":
     #gen_citationcount_per_topic()
 
     #gen_regressor_model()
-    citationcount_predict()
+    #citationcount_predict()
+    random_forest()
