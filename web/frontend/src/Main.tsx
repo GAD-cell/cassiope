00200ad7157ebaa139badc87cc7217c57e65d6a4
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-06-02 16:55:57 +0200

    web: Create backend

--- web/frontend/src/Main.tsx
+++ web/frontend/src/Main.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react'
+
 import React from 'react'
 
 const Main = () => {
+
+  const [time, setTime] = useState<number>(0)
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      console.log(data)
+      setTime(data.time)
+    });
+  }, [])
+
   return (
-    <div className=''>Main</div>
+    <div className=''>
+      <h1>Main</h1>
+
+      <p>Time: {time}</p>
+    </div>
   )
 }
 
--- web/start.sh
+++ web/start.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-# Start the web server
-cd frontend && npm start
+(
+    set -Ee
+
+    function _catch {
+        echo "Error: $1"
+        exit 0  # optional; use if you don't want to propagate (rethrow) error to outer shell
+    }
+    function _finally {
+        echo -e "\n\nCleaning up..."
+    }
+
+    trap _catch ERR
+    trap _finally EXIT
+    (trap 'kill 0' SIGINT;
+        cd backend && flask run --port=5001 &
+        cd frontend && npm start
+    )
+)
